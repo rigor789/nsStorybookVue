fc21f0df6cad9e9ed01c241d4eb2da514999bba7
Igor Randjelovic <rigor789@gmail.com>
2022-12-22 17:16:03 +0100

    chore: cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const { resolve } = require('path')
+
 module.exports = {
   stories: ["../**/*.stories.js"],
   addons: [
@@ -11,4 +13,28 @@ module.exports = {
     builder: "webpack5",
   },
   framework: "@storybook/vue",
+  webpackFinal (config) {
+    config.plugins = config.plugins.filter(plugin => {
+      const name = plugin.constructor.name;
+      return name !== 'VueLoaderPlugin';
+    })
+
+    config.module.rules = config.module.rules.filter(rule => {
+      const loader = rule.loader || ''
+      return !loader.includes('vue-loader');
+    })
+
+    config.module.rules.push({
+      test: /\.vue$/,
+      type: 'asset/source'
+    })
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '~': resolve(__dirname, '../src/'),
+    }
+
+    console.log(config)
+    return config
+  }
 };
